74330a1656aeb5798969765b5157c2691f594b10
revnandi <revnandi@gmail.com>
2024-07-02 14:58:16 +0200

    change middleware upload limits to default

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,17 +5,6 @@ export default [
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  {
-    name: "strapi::body",
-    config: {
-      formLimit: "32mb",
-      jsonLimit: "32mb",
-      textLimit: "32mb",
-      formidable: {
-        maxFileSize: 32 * 1024 * 1024,
-      },
-    },
-  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
